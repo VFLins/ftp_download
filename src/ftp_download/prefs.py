25e697d8cba5ed4e4724c9e5d5516a9ba2078541
Vitor Ferreira Lins <vflins@live.com>
2024-02-03 09:54:35 -0300

    update configs

--- src/ftp_download/prefs.py
+++ src/ftp_download/prefs.py
@@ -1,41 +1,45 @@
-from dataclasses import dataclass
 import asyncio
 import os
 
 class GlobalConfigDefaults():
     def __init__(self):
-        self.verbose: bool=False
+        self.verbose: bool = False
         """Should event messages be printed do `stdout`?"""
 
-        self.timeout: int=10
+        self.timeout: int = 10
         """Time to wait for request responses in seconds"""
 
-        self.retry: int=5
+        self.retry: int = 5
         """Maximum amount of attempts for every request"""
 
-        self.semaphore = asyncio.Semaphore(20)
-        """`asyncio.Semaphore` object that defines the maximum amount of simultaneous downloads"""
+        self.set_max_concurrent_jobs(20)
+        """`asyncio.Semaphore` object that defines the maximum amount of simultaneous downloads, defaults to 20"""
 
         self.raise_if_invalid: bool = True
-        """Raise error if invalid path provided, just print a message if `False`"""
+        """Raise error if invalid path provided, will only print a message if `False`"""
 
-        self.download_folder :str = self.get_default_download_folder()
+        self.download_folder :str = self.set_default_download_folder()
     
     def __repr__(self):
-        return f"""
-        Current settings are:
-
-        verbose: {self.verbose}
-        timeout time (s): {self.timeout}
-        retry (amount): {self.retry}
-        """
+        return (
+            "[Current ftp_download settings]\n" +
+            f"verbose = {self.verbose}\n" +
+            f"timeout = {self.timeout}\n" +
+            f"retry = {self.retry}\n" +
+            f"max_concurrent_jobs = {self.semaphore._value}\n" +
+            f"download_folder = {self.download_folder}"
+        )
     
-    def get_default_download_folder(self) -> str:
-        user_folder = os.path.expanduser("~")
-        return os.path.join(user_folder, "Downloads")
+    def set_default_download_folder(self, path: str | None=None) -> str:
+        """"""
+        if not path:
+            user_folder = os.path.expanduser("~")
+            setattr(self, "download_folder", os.path.join(user_folder, "Downloads"))
+        else:
+            setattr(self, "download_folder", path)
 
-    
+    def set_max_concurrent_jobs(self, amount) -> asyncio.Semaphore:
+        setattr(self, "semaphore", asyncio.Semaphore(amount))
 
-
-Conf = GlobalConfigDefaults()
+Configs = GlobalConfigDefaults()
 """Stores global configuration parameters, change it's parameter values to set your preferences"""
