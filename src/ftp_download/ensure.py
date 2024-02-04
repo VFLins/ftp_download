b00baf438292ed14694063fa905fe550bdebaead
Vitor Ferreira Lins <vflins@live.com>
2024-02-04 12:23:28 -0300

    flinted

--- src/ftp_download/ensure.py
+++ src/ftp_download/ensure.py
@@ -9,10 +9,9 @@ from pathlib import PureWindowsPath
 
 # IMPORTANT: https://kb.globalscape.com/KnowledgebaseArticle10142.aspx
 
-def posix_path(
-        path:       str
-    ) -> str:
-    
+
+def posix_path(path: str) -> str:
+
     """
     Normalizes a path and make compatible usual FTP file systems (unix-like).
 
@@ -23,70 +22,23 @@ def posix_path(
     Returns:
 
     A `str` with path normalized.
-    """
+    """ # noqa
 
     is_windows = (os.path.sep == "\\")
-    
+
     if is_windows:
         posix_path = PureWindowsPath(path).as_posix()
         swap_drive_letter_for_root = re.compile("[A-Z]:/", re.IGNORECASE)
         return swap_drive_letter_for_root.sub("/", posix_path)
-    
+
     else:
         return os.path.normpath(path)
 
-def change_remote_wd(
-        ftp:        ftplib.FTP, 
-        path:       str,
-        ) ->        bool:
-    
-    """
-    Tries to change directory on remote `ftp` server to `path` and returns a boolean signal indicating operation success.
 
-    Args:
-
-    - ftp (`ftplib.FPT`): An instance of the FTP class, expects to be already connected;
-    - path (`str`): FULL remote path targeted;
-
-    Returns:
-
-    `bool` indicating wether the operation was successful or not.
-    """
-
-    path = posix_path(path)
-    
-    for attempt in range(1, Conf.retry+1):
-        try:
-
-            if Conf.verbose:
-                print(f"connecting to {path}")
-
-            ftp.cwd(path)
-            if Conf.verbose:
-                print(
-                    f"Request completed after {attempt} attempts!"
-                    f"Now we are in {path}",
-                    sep="\n")
-            break
-
-        except ftplib.error_reply as reply:
-            if Conf.verbose:
-                print(
-                    f"Server respnded: {reply}",
-                    "Waiting...", sep="\n")
-            # TODO: include wait behaviour
-
-        # TODO: support other possible exceptions:
-        #        ftplib.error_temp, ftplib.error_perm, ftplib.error_proto, OSError and EOFError
-                
-        except Exception:
-            print("Unexpected error:\n", Exception)
-
-    current_path = posix_path(ftp.pwd())
-
-    return current_path == path
-
-def describe_dir(ftp: ftplib.FTP, path: str='') -> Dict[str, List[str]]:
+def describe_dir(
+        ftp: ftplib.FTP,
+        path: str = ''
+        ) -> Dict[str, List[str]]:
 
     # Capturing stdout adapted from:
     # https://stackoverflow.com/questions/5136611/capture-stdout-from-a-script
@@ -104,8 +56,8 @@ def describe_dir(ftp: ftplib.FTP, path: str='') -> Dict[str, List[str]]:
 
     # Will get wrong result if filename or dirname has space " "
     paths = {}
-    paths["dirs"] = [i.rpartition(" ")[-1] for i in lines_list if i[0]=="d"]
-    paths["files"] = [i.rpartition(" ")[-1] for i in lines_list if i[0]!="d"]
+    paths["dirs"] = [i.rpartition(" ")[-1] for i in lines_list if i[0] == "d"]
+    paths["files"] = [i.rpartition(" ")[-1] for i in lines_list if i[0] != "d"]
 
     if len(paths["dirs"]) + len(paths["files"]) == 0:
         error_msg = "Invalid path provided."
@@ -113,15 +65,15 @@ def describe_dir(ftp: ftplib.FTP, path: str='') -> Dict[str, List[str]]:
             raise ftplib.error_perm(error_msg)
         else:
             print(error_msg)
-            
+
     return paths
+
 
 def login(ftp: ftplib.FTP):
     try:
         ftp.login()
     except ftplib.error_perm as perm_err:
         resp = perm_err.__str__()
-        
+
         if resp[:3] != "530":
             raise ftplib.error_perm(resp)
-
