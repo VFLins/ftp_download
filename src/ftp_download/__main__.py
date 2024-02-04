c2c53bc68838c96b2ed0e5f948012f3279c4e0b3
Vitor Ferreira Lins <vflins@live.com>
2024-02-04 12:32:18 -0300

    flinted

--- src/ftp_download/__main__.py
+++ src/ftp_download/__main__.py
@@ -1,34 +1,35 @@
-from . import ensure 
+from . import ensure
 from . import timings
 from .prefs import Conf
 
 import os
 import re
 import asyncio
-import ftplib # https://github.com/python/cpython/blob/3.12/Lib/ftplib.py
+import ftplib  # https://github.com/python/cpython/blob/3.12/Lib/ftplib.py
 from typing import Optional, Sequence
 from shutil import unpack_archive
 
+
 def file(
-        ftp:                ftplib.FTP,
-        remote_file_path:   str,
-        local_path:         str=Conf.download_folder
+        ftp: ftplib.FTP,
+        remote_file_path: str,
+        local_path: str = Conf.download_folder
         ) -> None:
-     
+
     """
     Download a single file from a FTP server. Uses full path to file on the server, and full path to the download directory.
 
     Can use relative path to the file on the server, if `ftp.cwd('/path/to/folder/')` was called before.
-    
+
     Args:
 
     - ftp (`ftplib.FTP`): A `ftplib.FTP` object, expects to be already connected and logged in
     - remote_file_path (`str`):  Full path including filename of the file that will be downloaded
     - local_path (`str`): Full path to local directory where the file will be downloaded, will try to create folder if doesn't exists. Be careful with typos.
-    """
+    """ # noqa
 
     ensure.login(ftp=ftp)
-    
+
     if not os.path.exists(local_path):
         os.makedirs(local_path)
 
@@ -45,11 +46,12 @@ def file(
         print(f"Unexpected error downloading the file:\n{xpt}")
     loop.close()
 
+
 def from_folder(
-        ftp:                    ftplib.FTP, 
-        remote_path:            str, 
-        local_path:             str=Conf.download_folder,  
-        stops_with:             Optional[int]=None
+        ftp: ftplib.FTP,
+        remote_path: str,
+        local_path: str = Conf.download_folder,
+        stops_with: Optional[int] = None
         ):
 
     """
@@ -62,7 +64,7 @@ def from_folder(
     - local_path (`str`): Local path where files should be dumped;
     - max_concurrent_jobs (`int`): Maximum number of concurrent downloads. Defaults to 20;
     - stops_with (`int` or `None`): If not `None`, defines the maximum amount of files to be downloaded, otherwise download every file in `remote_path`. Defaults to `None`.
-    """
+    """ # noqa
 
     ensure.login(ftp=ftp)
 
@@ -71,7 +73,7 @@ def from_folder(
 
     if not os.path.exists(local_path):
         os.makedirs(local_path)
-    
+
     try:
         loop = asyncio.get_running_loop()
     except RuntimeError:
@@ -84,10 +86,12 @@ def from_folder(
         if Conf.verbose:
             print(f"Processing {idx}/{len(filenames)} files...", end="\r")
 
-        if (type(stops_with) == type(1)) and (stops_with == idx):
+        if stops_with == idx:
             break
 
-        remote_file_path = ensure.posix_path(os.path.join(remote_path, filename))
+        remote_file_path = ensure.posix_path(
+            os.path.join(remote_path, filename)
+        )
 
         task = timings.download_task(ftp, remote_file_path, local_path)
         tasks.append(task)
@@ -100,10 +104,10 @@ def from_folder(
 
 # TODO: Remove this soon
 def extract_and_organize(
-        path:           str, 
-        files_prefixes: Sequence[str]=["cons", "det"]
+        path: str,
+        files_prefixes: Sequence[str] = ["cons", "det"]
         ):
-    
+
     """
     Looks for sequences of characters on *.zip files, and extract the matched files to the same folder.
 
@@ -111,14 +115,14 @@ def extract_and_organize(
 
     - path (`str`): Path to look for .zip files;
     - files_prefixes (`Sequence[str]`): Any list-like with the sequences of charactes to match the file names, each element on the list will produce a folder. Is NOT case sensitive.
-    """
+    """ # noqa
 
     def filter_by_pattern(pattern, elements):
         matches = re.compile(pattern, re.IGNORECASE)
         return list(filter(matches.search, elements))
 
     path_of_files = os.listdir(path)
-    listings = {i:filter_by_pattern(i, path_of_files) for i in files_prefixes}
+    listings = {i: filter_by_pattern(i, path_of_files) for i in files_prefixes}
 
     for prefix in files_prefixes:
         test_path = os.path.join(path, prefix)
