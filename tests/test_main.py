dc48770c9c4431e4d4b04157b5f3005a95e971a5
Vitor Ferreira Lins <vflins@live.com>
2024-02-05 11:26:43 -0300

    add test for single file download

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,35 @@
 import pytest
-from ftp_download import (
-    file, 
-    from_folder
-)
+from ftplib import FTP
+import hashlib
+import os
+import ftp_download
 
 
 @pytest.mark.xfail(reason="Might fail due to network issues")
 def test_file():
-    pass
+
+    ftp = FTP("cran.r-project.org")
+    ftp.login()
+
+    lp = os.getcwd()
+    remote_file_path = "/pub/R/CRANlogo.png"
+    filename = os.path.split(remote_file_path)[1]
+
+    ftp_download.file(ftp, remote_file_path=remote_file_path, local_path=lp)
+
+    # get hash adapted from: https://stackoverflow.com
+    # /questions/16874598/how-do-i-calculate-the-md5-checksum-of-a-file-in-python
+
+    local_file_path = os.path.join(lp, filename)
+    with open(local_file_path, "rb") as f:
+        file_hash = hashlib.md5()
+        chunk = f.read()
+        while chunk:
+            file_hash.update(chunk)
+            chunk = f.read()
+
+    md5read = file_hash.hexdigest()
+    md5check = "c1c5f6b5f19ee2d8a5d0f9a3c612ae40"
+
+    os.remove(local_file_path)
+    assert md5read == md5check
