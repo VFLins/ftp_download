0c9d79b867f4c6cb987c8e6aca460d1e6095f654
Vitor Ferreira Lins <vflins@live.com>
2024-02-06 10:24:36 -0300

    include ftp_download.from_folder() test

--- tests/test_main.py
+++ tests/test_main.py
@@ -28,8 +28,44 @@ def test_file():
             file_hash.update(chunk)
             chunk = f.read()
 
-    md5read = file_hash.hexdigest()
-    md5check = "c1c5f6b5f19ee2d8a5d0f9a3c612ae40"
+    read = file_hash.hexdigest()
+    check = "c1c5f6b5f19ee2d8a5d0f9a3c612ae40"
 
     os.remove(local_file_path)
-    assert md5read == md5check
+    ftp.quit()
+
+    assert read == check
+
+
+@pytest.mark.xfail(reason="Might fail due to network issues")
+def test_from_folder():
+    ftp = FTP("cran.r-project.org")
+    ftp.login()
+
+    remote_path = "/pub/R/web"
+    lp = os.getcwd()
+    filenames = ftp_download.ensure.describe_dir(ftp, remote_path)['files']
+
+    ftp_download.from_folder(ftp=ftp, remote_path=remote_path, local_path=lp)
+
+    reads = []
+    for filename in filenames:
+        local_file_path = os.path.join(lp, filename)
+        with open(local_file_path, "rb") as f:
+            file_hash = hashlib.md5()
+            chunk = f.read()
+            while chunk:
+                file_hash.update(chunk)
+                chunk = f.read()
+
+        reads.append(file_hash.hexdigest())
+        os.remove(local_file_path)
+
+    ftp.quit()
+
+    checks = [
+        '2c6ecf143a56da19705976ae315e81f8',
+        '9467c77e7a6426087722c0712ed02b17'
+    ]
+    for read, check in zip(reads, checks):
+        assert read == check
